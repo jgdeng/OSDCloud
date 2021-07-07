348a94f63d5a26e4352eff96b3ad6b899c455111
jgdeng <jdeng@bergeys.com>
2021-07-06 21:18:22 -0400

    Update and rename Win10-pro-21H2.ps1 to Win10-pro-21H1.ps1

--- Win10-pro-21H1.ps1
+++ Win10-pro-21H1.ps1
@@ -17,7 +17,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "21H2" -OSEdition Pro -ZTI
+Start-OSDCloud -OSLanguage en-us -OSBuild "21H1" -OSEdition Pro -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
