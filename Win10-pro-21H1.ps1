c514f8d22e064562da9f49e1f6890ed963ab24c6
jgdeng <jdeng@bergeys.com>
2021-07-08 13:23:44 -0400

    Update Win10-pro-21H1.ps1

--- Win10-pro-21H1.ps1
+++ Win10-pro-21H1.ps1
@@ -17,7 +17,8 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "21H1" -OSEdition Pro -ZTI
+#Start-OSDCloud -OSLanguage en-us -OSBuild "21H1" -OSEdition Pro -ZTI
+Start-OSDCloud -ImageFileUrl "https://bergeysftp.com/WIN10.wim" (Get-FeatureUpdate).FileUrl - ImageIndex 3
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
